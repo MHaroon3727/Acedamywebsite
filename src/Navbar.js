d0ee907b58d5e93000123c18d24d86251cece852
MHaroon3727 <haroonrasheed211f@gmail.com>
2023-08-19 01:25:46 -0700

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="google.com">About</a>
+        </li>
+        <li>
+          <a>Services</a>
+        </li>
+        <li>
+          <a>contact</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
